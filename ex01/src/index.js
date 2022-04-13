8f86133bd7889b9d16160101d513b58d054703f0
Haris Pasic <harispasic@ymail.com>
2022-04-14 01:48:12 +0200

    Spread operator

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,11 @@
+const array1 = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
 
+function main(arr){
+    let array2 = [...array1];
+
+    return array2;
+}
+
+console.log(main(array1));
+
+module.exports = main;
